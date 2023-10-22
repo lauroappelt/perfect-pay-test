923c74eeece6ecfd04ead9fa8283e20895e47bda
lauro <lauro.appelt@universo.univates.br>
2023-10-22 17:00:58 -0300

    created tests for create sale endpoint

--- tests/Feature/Sale/CreateSaleTest.php
+++ tests/Feature/Sale/CreateSaleTest.php
@@ -8,6 +8,7 @@ use App\Models\User;
 use App\Models\Costomer\Costomer;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Ramsey\Uuid\Uuid;
 use Tests\TestCase;
 
 class CreateSaleTest extends TestCase
@@ -21,7 +22,7 @@ class CreateSaleTest extends TestCase
         $this->user = User::factory()->create();
     }
 
-    public function test_an_create_new_sale(): void
+    public function test_can_create_new_sale(): void
     {
         $product = Product::factory()->create();
 
@@ -34,7 +35,7 @@ class CreateSaleTest extends TestCase
             'product_id' => $product->id,
             'ammount' => 2,
             'discount' => 0,
-        ]);
+        ],['Accept' => 'application/json']);
 
         $response->decodeResponseJson();
 
@@ -60,5 +61,57 @@ class CreateSaleTest extends TestCase
         $this->assertEquals(now('America/Sao_Paulo')->format('Y-m-d'), $sale['date']);
 
         $this->assertDatabaseHas('sales', $sale);
+    }
+
+    public function test_cannot_create_sale_with_non_existing_product(): void
+    {
+        $costomer = Costomer::factory()->create();
+
+        $this->actingAs($this->user);
+
+        $response = $this->post(route('api.sale.create'), [
+            'costomer_id' => $costomer->id,
+            'product_id' => Uuid::uuid4(),
+            'ammount' => 2,
+            'discount' => 0,
+        ], ['Accept' => 'application/json']);
+
+        $response->assertUnprocessable();
+    }
+
+    public function test_cannot_create_sale_with_non_existing_costomer(): void
+    {
+        $product = Product::factory()->create();
+
+        $this->actingAs($this->user);
+
+        $response = $this->post(route('api.sale.create'), [
+            'costomer_id' => Uuid::uuid4(),
+            'product_id' => $product->id,
+            'ammount' => 2,
+            'discount' => 0,
+        ], ['Accept' => 'application/json']);
+
+        $response->assertUnprocessable();
+    }
+
+    public function test_cannot_create_sale_with_ammount_less_than_one(): void
+    {
+        $product = Product::factory()->create();
+
+        $costomer = Costomer::factory()->create();
+
+        $this->actingAs($this->user);
+
+        $response = $this->post(route('api.sale.create'), [
+            'costomer_id' => $costomer->id,
+            'product_id' => $product->id,
+            'ammount' => -1,
+            'discount' => 0,
+        ],['Accept' => 'application/json']);
+
+        $response->decodeResponseJson();
+
+        $response->assertUnprocessable();
     }
 }
